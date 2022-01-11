9033996c2c460181c6b4cf0c441aa331752f27af
AlejandroSchwartz <alejandromatiassch@gmail.com>
2022-01-11 10:28:20 -0300

    Agregando la configuracion para desplegar en Netlify

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve( __dirname , 'build'),
+        path: path.resolve( __dirname , 'dist'),
         filename: 'bundle.js',
         publicPath: "/",
     },
